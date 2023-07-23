4ea27413e17002a62a92beb38d705fdad6a8f817
cheeta9 <cheeta9.mail@gmail.com>
2023-07-23 15:05:23 +0900

    add/colorscheme

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,10 +1,25 @@
-vim.cmd.packadd "packer.nvim"
+vim.cmd.packadd 'packer.nvim'
 
-require("packer").startup(function()
-  -- 起動時に読み込むプラグイン
-  use "tpope/vim-fugitive"
+require('packer').startup(function()
+  -- colorscheme
+  use {
+    'morhetz/gruvbox',
+    config = function()
+      vim.cmd.colorscheme("gruvbox")
+      -- highlight settings
+      vim.cmd [[
+        hi Normal ctermbg=none
+        hi NonText ctermbg=none
+        hi LineNr ctermbg=none
+        hi Folded ctermbg=none
+        hi EndOfBuffer ctermbg=none
+        hi CursorLineNr ctermbg=5 ctermfg=255
+        hi clear CursorLine
+      ]]
+    end
+  }
+  use 'tpope/vim-fugitive'
 
-  -- 遅延読み込みのプラグイン
-  use { "wbthomason/packer.nvim", opt = true }
+  use { 'wbthomason/packer.nvim', opt = true }
 end)
 
