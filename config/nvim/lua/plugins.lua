9b4ee7731bd8f729b36805186d4f0fb839d231c2
cheeta9 <cheeta9.mail@gmail.com>
2023-07-23 16:05:59 +0900

    add/vim-git settings

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -27,7 +27,33 @@ require('packer').startup(function()
     end
   }
 
-  use 'tpope/vim-fugitive'
+  -- Gitのステータスをファイル左横に表示するやつ
+  use {
+    'airblade/vim-gitgutter',
+    config = function()
+      vim.opt.updatetime = 100
+      vim.keymap.set('n', ']h', '<Plug>(GitGutterNextHunk)')
+      vim.keymap.set('n', '[h', '<Plug>(GitGutterPrevHunk)')
+      vim.keymap.set('n', 'ghs', '<Plug>(GitGutterStageHunk)')
+      vim.keymap.set('n', 'ghu', '<Plug>(GitGutterUndoHunk)')
+      vim.keymap.set('n', 'ghp', '<Plug>(GitGutterPreviewHunk)')
+    end
+  }
+
+  -- vim上でGit操作可能にするやつ
+  use {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<Leader>ga', ':G add %:p<CR><CR>')
+      vim.keymap.set('n', '<Leader>gc', ':G commit<CR><CR>')
+      vim.keymap.set('n', '<Leader>gs', ':G status<CR>')
+      vim.keymap.set('n', '<Leader>gp', ':G push<CR>')
+      vim.keymap.set('n', '<Leader>gd', ':G diff<CR>')
+      vim.keymap.set('n', '<Leader>gl', ':G log<CR>')
+      vim.keymap.set('n', '<Leader>glo', ':G log --oneline<CR>')
+      vim.keymap.set('n', '<Leader>gb', ':G blame<CR>')
+    end
+  }
 
   use { 'wbthomason/packer.nvim', opt = true }
 end)
