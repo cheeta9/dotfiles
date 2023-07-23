c460a2a6a0793ea959181f10ab56046ba1615f08
cheeta9 <cheeta9.mail@gmail.com>
2023-07-23 17:13:25 +0900

    add/useful nvim plugins

---
+++ config/nvim/after/plugin/easymotion.rc.lua
@@ -0,0 +1,8 @@
+vim.keymap.set('', '<Leader>f', '<Plug>(easymotion-bd-f)')
+vim.keymap.set('n', '<Leader>f', '<Plug>(easymotion-overwin-f)')
+vim.keymap.set('n', '<Leader>s', '<Plug>(easymotion-overwin-f2)')
+vim.keymap.set('', '<Leader>L', '<Plug>(easymotion-bd-jk)')
+vim.keymap.set('n', '<Leader>L', '<Plug>(easymotion-overwin-line)')
+vim.keymap.set('', '<Leader>w', '<Plug>(easymotion-bd-w)')
+vim.keymap.set('n', '<Leader>w', '<Plug>(easymotion-overwin-w)')
+
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -14,6 +14,49 @@ require('packer').startup(function()
   use 'airblade/vim-gitgutter'
   -- vim上でGit操作可能にするやつ
   use 'tpope/vim-fugitive'
+  -- フォーカス移動を自由自在にするやつ
+  use 'easymotion/vim-easymotion'
+  -- yank範囲をハイライトするやつ
+  use 'machakann/vim-highlightedyank'
+  -- アイコンを提供するやつ
+  use 'kyazdani42/nvim-web-devicons'
+  -- タブ操作を簡単にするやつ
+  use 'romgrk/barbar.nvim'
+  -- 「閉じ」文字を補完するやつ
+  use 'cohama/lexima.vim'
+  -- 「囲み」文字を簡単に操作するやつ
+  use 'tpope/vim-surround'
+  -- 縦スクロールバーを表示するやつ
+  use 'dstein64/nvim-scrollview'
+
+  -- 選択範囲を段階的に広げるやつ
+  use {
+    'terryma/vim-expand-region',
+    config = function()
+      vim.keymap.set('', 'v', '<Plug>(expand_region_expand)')
+      vim.keymap.set('', 'V', '<Plug>(expand_region_shrink)')
+    end
+  }
+  -- JSONを扱いやすくするやつ
+  use {
+    'elzr/vim-json',
+    config = function()
+      vim.opt.conceallevel = 2
+      vim.opt.concealcursor = 'niv'
+      vim.g.vim_json_syntax_conceal = 0
+    end
+  }
+  -- コメントアウト・解除を簡単にするやつ
+  use {
+    'preservim/nerdcommenter',
+    config = function()
+      vim.g.NERDSpaceDelims = 1
+      vim.g.NERDCompactSexyComs = 1
+      vim.g.NERDDefaultAlign = 'left'
+      vim.keymap.set('n', '<C-c>', '<Plug>NERDCommenterToggle')
+      vim.keymap.set('v', '<C-c>', '<Plug>NERDCommenterToggle<CR>gv')
+    end
+  }
 
   use { 'wbthomason/packer.nvim', opt = true }
 end)
