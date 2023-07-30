fcc1f2dbce6bc04aec4bcd62d2f25e74b5db6aa1
cheeta9 <cheeta9.mail@gmail.com>
2023-07-30 11:11:54 +0900

    fix/telescope.nvim settings

--- config/nvim/after/plugin/telescope.rc.lua
+++ config/nvim/after/plugin/telescope.rc.lua
@@ -3,20 +3,44 @@ if (not status) then
   return
 end
 
+local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
-telescope.setup({
+telescope.setup{
   defaults = {
     file_ignore_patterns = {'^.git/', '^node_modules/'},
     layout_strategy = 'vertical',
     layout_config = { height = 0.8, width = 0.8 },
-    sorting_strategy = 'ascending'
+    sorting_strategy = 'ascending',
+    mappings = {
+      i = { ['<Leader>q'] = actions.close }
+    }
   }
-})
+}
 telescope.load_extension('frecency')
 
 vim.keymap.set('n', '<C-p>', function()
   builtin.find_files({ no_ignore = false, hidden = true })
+end)
+
+vim.keymap.set('n', ',e', function()
+  local function is_git_repo()
+    vim.fn.system('git rev-parse --is-inside-work-tree')
+    return vim.v.shell_error == 0
+  end
+  local function get_git_root()
+    local dot_git_path = vim.fn.finddir('.git', '.;')
+    return vim.fn.fnamemodify(dot_git_path, ':h')
+  end
+  local opts = {}
+  if is_git_repo() then
+    opts = { cwd = get_git_root() }
+  end
+  builtin.find_files(opts)
+end)
+
+vim.keymap.set('n', ',b', function()
+  builtin.buffers()
 end)
 
 vim.keymap.set('n', ',r', function()
@@ -27,7 +51,11 @@ vim.keymap.set('n', ',a', function()
   builtin.diagnostics()
 end)
 
-vim.keymap.set('n', '<C-[>', "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>", {
+vim.keymap.set('n', ',q', function()
+  builtin.quickfix()
+end)
+
+vim.keymap.set('n', ',f', "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>", {
   noremap = true,
   silent = true
 })
